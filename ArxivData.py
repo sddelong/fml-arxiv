a1f4e3c1c91961c0921a2a1c9d52a3ad4d36b2f5
Steven Delong <steven.d.delong@gmail.com>
2014-04-03 18:11:12 -0400

    small edits to arxivdata.py

--- ArxivData.py
+++ ArxivData.py
@@ -5,31 +5,6 @@ from xml.etree import ElementTree as ET
 
 
 
-url = 'http://export.arxiv.org/api/query?search_query=all:{0}&start=0&max_results=5'.format(sys.argv[1])
-
-data = urllib.urlopen(url).read()
-
-#remove namespaces
-data = re.sub(' xmlns="[^"]+"', '', data, count=1)
-
-root = ET.fromstring(data)
-
-for entry in root.findall('entry'):
-    print "--------------------------"
-    
-    summary = entry.find('summary').text
-    title = entry.find('title').text
-    print "Title:", title
-    print "  "
-    print "Authors:"
-    authors = entry.findall('author')
-    for author in authors:
-        name = author.find('name').text
-        print name
-    print "  "
-    print "Abstract:"
-    print summary
-    print "  "
 
 class Paper:
     """ Class to hold paper information.  For now this is blank.  Could be useful later."""
@@ -40,9 +15,35 @@ class Paper:
         
         
         
+if __name__ == "__main__":
 
+    url = 'http://export.arxiv.org/api/query?search_query=all:{0}&start=0&max_results=5'.format(sys.argv[1])
 
+    data = urllib.urlopen(url).read()
 
-
-
-
+#remove namespaces
+    data = re.sub(' xmlns="[^"]+"', '', data, count=1)
+    
+    root = ET.fromstring(data)
+    
+    for entry in root.findall('entry'):
+        print "--------------------------"
+        
+        summary = entry.find('summary').text
+        title = entry.find('title').text
+        print "Title:", title
+        print "  "
+        print "Authors:"
+        authors = entry.findall('author')
+        for author in authors:
+            name = author.find('name').text
+            print name
+        print "  "
+        print "Abstract:"
+        print summary
+        print "  "
+            
+            
+            
+            
+            
