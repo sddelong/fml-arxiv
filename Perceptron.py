2e02cb35143bd74c1639ba3b0b6c4faa32c1d2e0
Steven <steven.d.delong@gmail.com>
2014-04-10 15:19:06 -0400

    Added very basic Perceptron classifier, and made a batch training example.

---
+++ Perceptron.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+def sign(x):
+    """ compute sign of x, with convention that 0 is positive """
+    if x >= 0:
+        return 1
+    else:
+        return -1
+
+
+
+class PerceptronClassifier:
+    """ Simple perceptron algorithm for online learning. """
+    
+    def __init__(self,n,eta):
+        """ Constructor for Perceptron. 
+
+           Arguments:
+                   eta - learning rate
+                   n - length of input data. (We may want to do this differently
+                            to be specific to bags of words)
+        """
+        
+        self.eta = eta
+        self.weights = np.zeros(n)
+        self.n_correct = 0
+        self.n_wrong = 0
+        
+        
+    def Update(self,x,y_hat,y):
+        """ given that the Perceptron has guessed y_hat for data x, update it 
+        using the correct value y.
+
+        Note: must be used after every prediction to obtain a correct accuracy estimate
+        """
+        
+        if y_hat != y:
+            #update
+            self.n_wrong += 1
+            self.weights = self.weights + self.eta*y*x
+        else:
+            self.n_correct += 1
+            
+        
+        return
+        
+
+    def Predict(self,x):
+        """ predict y by using sign of a dot product of x with weights """
+        
+        return sign(np.dot(self.weights,x))
+
+    def ReportAccuracy(self):
+        
+        return float(self.n_correct)/float(self.n_wrong + self.n_correct)
+
+
+if __name__ == "__main__":
+    
+
+    x = np.ones(10)
+    pclassifier = Perceptron(10,0.3)
+    
+    y_hat = pclassifier.Predict(x)
+    
+    assert y_hat == 1, "y_hat should be 1 at initialization, it is not"
+    
+    pclassifier.Update(x,y_hat,-1)
+    y_hat_2 = pclassifier.Predict(x)
+    
+    assert y_hat_2 == -1, "y_hat should be -1 after update, it is not"
+    
+    
+    print "Completed some basic tests"
+
+
+
+
+        
+
+        
+           
+        
---
+++ TrainArxivBatch.py
@@ -0,0 +1,86 @@
+"""
+Script that grabs arxiv papers matching two different strings and trains a perceptron classifier
+using bag of words from the abstract to identify which of the subjects the article is on 
+run with:
+
+python TrainArxivBatch <string1> <string2>
+
+"""
+
+from ArxivData import GetDatedPapers
+from ArxivData import SearchPapers
+from ArxivData import GetAbstracts
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from Perceptron import PerceptronClassifier
+import numpy as np
+import sys
+
+
+if __name__ == "__main__":
+
+
+    #parameters
+    n_negative = 80
+    n_positive = 80
+    
+    #random permutation of indices, so 
+    # the algorithm doesn't see data all positive then all negative
+    idxs = np.random.permutation(n_negative + n_positive)
+
+    #test positive results, Brownian Dynamics
+    paper_list = SearchPapers(sys.argv[1],n_positive)
+    abstracts = GetAbstracts(paper_list)
+
+    # grab a bunch of physics papers
+    paper_list = SearchPapers(sys.argv[2],n_negative)
+    vectorizer = TfidfVectorizer(stop_words='english')
+    abstracts.extend(GetAbstracts(paper_list))
+
+    #permute abstracts in order of indices, then make a vector of 
+    # word features
+    abstracts = np.array(abstracts)[idxs]
+    abstracts = list(abstracts)
+    abstract_vectors = vectorizer.fit_transform(abstracts)
+
+    #create target data and permute
+    target_vector = np.concatenate((np.ones(n_positive),-1.*np.ones(n_negative)))
+    target_vector = target_vector[idxs]
+    
+    #create perceptron classifier
+    perceptron_classifier = PerceptronClassifier(abstract_vectors.shape[1],0.3)
+    
+    for k in range(abstract_vectors.shape[0]):
+        x = []
+        for j in range(abstract_vectors.shape[1]):
+            x.append(abstract_vectors[(k,j)])
+
+        x = np.array(x)
+                     
+        y_hat = perceptron_classifier.Predict(x)
+        print "-"*80
+        if y_hat == 1:
+            print "  "
+            print "Perceptron indicates that this article is about {0}".format(sys.argv[1])
+            print "  "
+        else:
+            print "  "
+            print "Perceptron indicates that this article is about {0}".format(sys.argv[2])
+            print "  "
+            
+        print abstracts[k]
+        print "-"*80
+        y = target_vector[k]
+        perceptron_classifier.Update(x,y_hat,y)
+    
+    print "Accuracy of Perceptron is ", perceptron_classifier.ReportAccuracy()
+
+        
+    
+
+    
+    
+    
+
+    
+    
