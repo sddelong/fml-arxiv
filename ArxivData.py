0165a03111f4c220aef1cf3b6e808c182a667a60
Steven Delong <steven.d.delong@gmail.com>
2014-04-21 19:06:28 -0400

    updating to latest.

--- ArxivData.py
+++ ArxivData.py
@@ -6,6 +6,7 @@ import re #for removing namespaces
 import sys
 from xml.etree import ElementTree as ET
 from sklearn.feature_extraction.text import TfidfVectorizer
+import time
 
 class Paper:
     """ Class to hold paper information.
@@ -17,18 +18,44 @@ class Paper:
                published - string, date and time the paper was published.
     """
 
-    def __init__(self,entry):
-        #blank
-        self.title = entry.find('title').text
-        self.abstract = entry.find('summary').text
+    def __init__(self,entry,xml_type):
         
-        self.authors = []
-        authors = entry.findall('author')
-        for author in authors:
-            name = author.find('name').text
-            self.authors.append(name)
+        """create a paper object from an XML entry, which could be returned 
+        from a query using the arxiv api, in which case specify xml_type == "query", 
+        or it could be from an rss feed, in which case specify xml_type == "rss"
+        """
+        
+        if xml_type == "query":
+            self.title = entry.find('title').text
+            self.abstract = entry.find('summary').text
+        
+            self.authors = []
+            authors = entry.findall('author')
+            for author in authors:
+                name = author.find('name').text
+                self.authors.append(name)
 
-        self.published = entry.find('published').text
+            self.published = entry.find('published').text
+
+        elif xml_type == "rss":
+            self.title = entry.find('title').text
+            self.abstract = entry.find('description').text
+        
+            self.authors = []
+            authors = entry.find('dc:creator').text
+            #TODO: Parse this.
+
+#            for author in authors:
+#                name = author.find('name').text
+#                self.authors.append(name)
+
+            self.published = None # TODO(delong) Make this todays date.
+
+        else:
+            print "Error: xml_type must be one of: query, rss"
+            raise NotImplementedError
+        
+
 
     def Print(self):
         print "--------------------------"
@@ -70,7 +97,7 @@ def SearchPapers(input_str,max_results=5):
     root = ET.fromstring(data)
     
     for entry in root.findall('entry'):
-        paper_list.append(Paper(entry))
+        paper_list.append(Paper(entry,"query"))
     
     return paper_list
 
@@ -104,40 +131,79 @@ def PrintEntry(entry):
 
     return
 
-def GetDatedPapers(date,max_results = None):
-    """ 
-    GetDatedPapers(date,max_results)
-    
-    Function to get all arxiv papers published on a given date.  
+def IDFromLink(link):
+    """
+    quick helper function to get an article id from a string of its link
     
     inputs:
-          date - this function will return all papers on arxiv uploaded on this specified date
-          max_results - optional upper limit on number of papers returned.
+         link - string link to a paper, assumed to be of 
+                the form http://arxiv.org/abs/<paper-id>
+    outputs:
+         paper_id - string with just the id
+    """
+    paper_id = link[21:]
+
+    return paper_id
+    
+
+def GetTodaysPapers():
+    """ 
+    GetTodaysPapers()
+    
+    Function to get all arxiv papers published on the current day.
+    
+    inputs:
+            <none>
 
     return value:
            This function returns a list of Paper objects, populated from the papers on Arxiv
     """
 
+    #list of all subjects to grab papers from
+    subject_list = ["cs","math","physics","cond-mat","math-ph","stat"]
+
     #initialize paper list
+    id_list = []
     paper_list = []
     
-    if(max_results):
-        url = 'http://export.arxiv.org/api/query?search_query=published:&start=0&max_results={1}'.format(date,max_results)
-    else:
-        url = 'http://export.arxiv.org/api/query?search_query=published:{0}&start=0'.format(date)
+    url = "http://export.arxiv.org/rss/math"
+
 
     #extract info here.
-
     data = urllib.urlopen(url).read()
-
+    
     #remove namespaces
     data = re.sub(' xmlns="[^"]+"', '', data, count=1)
     
     root = ET.fromstring(data)
 
-    for entry in root.findall('entry'):
-        paper_list.append(Paper(entry))
+    for item in root.findall('item'):
+        id_list.append(IDFromLink(item.find('link').text))
 
+    
+    query_str = "id:" + str(id_list[0])
+    for k in range(1,len(id_list)):
+        query_str = query_str + "+OR+id:" + str(id_list[k])
+    
+    #now get URL for all papers we want TODO: don't hardcode length, make this work
+    url = 'http://export.arxiv.org/api/query?search_query={0}&max_results=300'.format(query_str)
+    
+    #get xml data
+    data = urllib.urlopen(url).read()
+    #remove namespace
+    data = re.sub(' xmlns="[^"]+"', '', data, count=1)
+    # get root
+    root = ET.fromstring(data)
+    
+    #create list of papers
+    for entry in root.findall('entry'):
+        this_paper = Paper(entry,"query")
+        print "this paper published", this_paper.published
+        print "todays date", time.strftime("%Y-%m-%d")
+        #check that the paper was published today, not just updated.
+        if this_paper.published[0:10] == time.strftime("%Y-%m-%d"):
+            paper_list.append(this_paper)
+            
     return paper_list
 
 
@@ -161,16 +227,18 @@ def GetAbstracts(paper_list):
 
 if __name__ == "__main__":
 
-    paper_list = SearchPapers(sys.argv[1],20)
+#    paper_list = SearchPapers(sys.argv[1],20)
+    paper_list = GetTodaysPapers()
 
-#    for paper in paper_list:
-#        paper.Print()
+    print len(paper_list)
+    for paper in paper_list:
+        paper.Print()
         
     vectorizer = TfidfVectorizer()
     abstracts = GetAbstracts(paper_list)
 
 #    print abstracts
-    abstract_vectors = vectorizer.fit_transform(abstracts)
-    print abstract_vectors.shape
-    print abstract_vectors.nnz/float(abstract_vectors.shape[0])
+#    abstract_vectors = vectorizer.fit_transform(abstracts)
+#    print abstract_vectors.shape
+#    print abstract_vectors.nnz/float(abstract_vectors.shape[0])
                           
