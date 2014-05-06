5e33948d18f151e8c4703bc28b65e2ed51f89922
Joey McDonald <josephpmcdonald@gmail.com>
2014-05-05 22:09:51 -0400

    added getTestData for real

---
+++ getTestData.py
@@ -0,0 +1,15 @@
+import ArxivData as ad
+from cPickle import dump
+
+paper_list = []
+
+if __name__ == '__main__':
+    paper_list = (ad.GetPapersOAI('2014-04-30','2014-05-02','math')
+        +ad.GetPapersOAI('2014-04-30','2014-05-02','cs'))
+
+with open('TestData.pkl','w') as file:
+    for paper in paper_list:
+        dump(paper,file)
+
+
+
